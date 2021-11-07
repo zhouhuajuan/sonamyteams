cad3d6c67ee516ec2f596488ecf693c446a564e7
3219005498周华娟 <1247054987@qq.com>
2021-11-08 00:44:54 +0800

    第一次提交：创建springboot项目

---
+++ anonymousletter/src/main/java/com/somanyteam/event/AnonymousletterApplication.java
@@ -0,0 +1,13 @@
+package com.somanyteam.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousletterApplication.class, args);
+    }
+
+}
---
+++ anonymousletter/src/test/java/com/somanyteam/event/AnonymousletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somanyteam.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
