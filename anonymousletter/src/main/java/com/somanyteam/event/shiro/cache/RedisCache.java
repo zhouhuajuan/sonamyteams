a288413edbf2ed97c5aaf64d2c47c399faf40fc7
3219005497郑宝柔 <1097641461@qq.com>
2021-11-09 22:34:45 +0800

    [zbr] 添加shiro配置，添加登录功能

---
+++ anonymousletter/src/main/java/com/somanyteam/event/shiro/cache/RedisCache.java
@@ -0,0 +1,79 @@
+package com.somanyteam.event.shiro.cache;
+
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.somanyteam.event.util.ApplicationContextUtils;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author zbr
+ */
+//自定义redis缓存的实现
+public class RedisCache<k,v> implements Cache<k,v> {
+
+    private String cacheName;
+
+
+    public RedisCache() {
+    }
+
+    public RedisCache(String cacheName) {
+        this.cacheName = cacheName;
+    }
+
+    @Override
+    public v get(k k) throws CacheException {
+        System.out.println("get key:"+k);
+        return (v) getRedisTemplate().opsForHash().get(this.cacheName,k.toString());
+    }
+
+    @Override
+    public v put(k k, v v) throws CacheException {
+        System.out.println("put key: "+k);
+        System.out.println("put value:"+v);
+        getRedisTemplate().opsForHash().put(this.cacheName,k.toString(),v);
+        return null;
+    }
+
+    @Override
+    public v remove(k k) throws CacheException {
+        System.out.println("=============remove=============");
+        return (v) getRedisTemplate().opsForHash().delete(this.cacheName,k.toString());
+    }
+
+    @Override
+    public void clear() throws CacheException {
+        System.out.println("=============clear==============");
+        getRedisTemplate().delete(this.cacheName);
+    }
+
+    @Override
+    public int size() {
+        return getRedisTemplate().opsForHash().size(this.cacheName).intValue();
+    }
+
+    @Override
+    public Set<k> keys() {
+        return getRedisTemplate().opsForHash().keys(this.cacheName);
+    }
+
+    @Override
+    public Collection<v> values() {
+        return getRedisTemplate().opsForHash().values(this.cacheName);
+    }
+
+    private RedisTemplate getRedisTemplate(){
+        RedisTemplate redisTemplate = (RedisTemplate) ApplicationContextUtils.getBean("redisTemplate");
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
---
+++ anonymousletter/src/main/java/com/somanyteam/event/shiro/cache/RedisCacheManager.java
@@ -0,0 +1,19 @@
+package com.somanyteam.event.shiro.cache;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.CacheManager;
+
+/**
+ * @author zbr
+ */
+//自定义shiro缓存管理器
+public class RedisCacheManager implements CacheManager {
+
+    //参数1:认证或者是授权缓存的统一名称
+    @Override
+    public <K, V> Cache<K, V> getCache(String cacheName) throws CacheException {
+        System.out.println(cacheName);
+        return new RedisCache<K,V>(cacheName);
+    }
+}
